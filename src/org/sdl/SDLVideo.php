dabafb14d7205e4536f58f508e5c6e256979b620
Steffen Kowalski <scipper@myscipper.de>
2014-11-25 11:55:41 +0100

    Create SDLVideo.php

---
+++ src/org/sdl/SDLVideo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace org\sdl;
+
+/**
+ * 
+ * @author Steffen Kowalski <sk@traiwi.de>
+ *
+ * @since 24.11.2014
+ * @namespace org\sdl
+ * @package org\sdl
+ *
+ */
+class SDLVideo {
+	
+	/**
+	 * 
+	 * @return SDL_Surface
+	 */
+	public static function getVideoSurface() {
+		return SDL_GetVideoSurface();
+	}
+	
+}
+
+?>
