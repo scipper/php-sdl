fb9ed52022015f7334f9bcaa19b0386893569a6c
Steffen Kowalski <scipper@myscipper.de>
2014-11-25 11:55:30 +0100

    Create SDLSurface.php

---
+++ src/org/sdl/SDLSurface.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace org\sdl;
+
+/**
+ * 
+ * @author Steffen Kowalski <sk@traiwi.de>
+ *
+ * @since 25.11.2014
+ * @namespace org\sdl
+ * @package org\sdl
+ *
+ */
+class SDLSurface {
+	
+	const ANYFORMAT = SDL_ANYFORMAT;
+	
+	/**
+	 * 
+	 * @var \SDL_Surface
+	 */
+	protected $surface;
+	
+	
+	/**
+	 * 
+	 * @param \SDL_Surface $surface
+	 */
+	public function __construct($surface) {
+		$this->surface = $surface;
+	}
+
+	/**
+	 * 
+	 * @return SDL_Surface
+	 */
+	public function get() {
+		return $this->surface;
+	}
+	
+}
+
+?>
