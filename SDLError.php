97230884df038265e04b01aa10e364ae092eb95f
scipper <scipper@myscipper.de>
2014-11-26 21:56:22 +0100

    error und palette

---
+++ SDLError.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace org\sdl;
+
+/**
+ * 
+ * @author Steffen Kowalski <sk@traiwi.de>
+ *
+ * @since 26.11.2014
+ * @namespace org\sdl
+ * @package org\sdl
+ *
+ */
+class SDLError {
+	
+	
+	
+}
+
+?>
--- SDLPalette.php
+++ SDLPalette.php
@@ -2,6 +2,15 @@
 
 namespace org\sdl;
 
+/**
+ * 
+ * @author Steffen Kowalski <sk@traiwi.de>
+ *
+ * @since 26.11.2014
+ * @namespace org\sdl
+ * @package org\sdl
+ *
+ */
 class SDLPalette {
 	
 	
