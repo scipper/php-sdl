0404e56d09876571312c9d4b3aee57496301ba8a
scipper <scipper@myscipper.de>
2014-11-30 19:49:49 +0100

    mods hinzugefügt

--- events/SDLMod.php
+++ events/SDLMod.php
@@ -13,7 +13,37 @@ namespace org\sdl\events;
  */
 class SDLMod {
 	
+	const MOD_NONE = KMOD_NONE;
 	
+	const MOD_LSHIFT = KMOD_LSHIFT;
+	
+	const MOD_RSHIFT = KMOD_RSHIFT;
+	
+	const MOD_LCTRL = KMOD_LCTRL;
+	
+	const MOD_RCTRL = KMOD_RCTRL;
+	
+	const MOD_LALT = KMOD_LALT;
+	
+	const MOD_RALT = KMOD_RALT;
+	
+	const MOD_LMETA = KMOD_LMETA;
+	
+	const MOD_RMETA = KMOD_RMETA;
+	
+	const MOD_NUM = KMOD_NUM;
+	
+	const MOD_CAPS = KMOD_CAPS;
+	
+	const MOD_MODE = KMOD_MODE;
+	
+	const MOD_CTRL = KMOD_CTRL;
+	
+	const MOD_SHIFT = KMOD_SHIFT;
+	
+	const MOD_ALT = KMOD_ALT;
+	
+	const MOD_META = KMOD_META;
 	
 }
 
